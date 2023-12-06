82e07f431a38244ae31ec56598a3595ef4146db0
Trishane <trishaneww@gmail.com>
2023-12-05 19:50:07 -0500

    new commit

---
+++ practice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // standard character output
+    // std::cout << "I like pizza!" << std::endl;
+    // std::cout << "Its really good" << std::endl;
+    // std::cout << "END OF LINE" << std::endl;
+    // std::cout << 1+2 << std::endl;
+
+    int x; //declaration
+    x = 5;
+
+    std::cout << x;
+    return 0;
+}
