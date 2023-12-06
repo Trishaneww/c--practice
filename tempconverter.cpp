1b3333a22f8a8d129a9b937427537ff657eb1254
Trishane <trishaneww@gmail.com>
2023-12-06 17:39:33 -0500

    updated conditions

--- tempconverter.cpp
+++ tempconverter.cpp
@@ -2,28 +2,25 @@
 #include <cmath>
 
 int main () {
-
     double temp;
     char unit;
 
     std::cout << "*************** temp converter ******************" << '\n';
-    std::cout << "enter temperature ";
-    std::cin >> temp;
     std::cout << "enter unit ";
     std::cin >> unit;
 
-    switch(unit) {
-        case 'c':
-            std::cout << "your temp in farenhheit is " << (temp * 9/5) + 32;
-            break;
-        case 'f':
-            std::cout << "your temp in celsius is " << (temp - 32) * 5/9;
-            break;
-        default:
-            std::cout << "enter a valud unit";
-            break;
-    }
 
+    if(unit == 'f' || unit == 'F') {
+         std::cout << "enter temperature ";
+         std::cin >> temp;
+         std::cout << "your temp in celsius is " << (temp - 32) * 5/9;
+    }else if (unit == 'c' || unit == 'C') {
+         std::cout << "enter temperature ";
+         std::cin >> temp;
+         std::cout << "your temp in farenhheit is " << (temp * 9/5) + 32;
+    } else {
+        std::cout << "ENTER A VALID UNIT";
+    }
 
     return 0;
 }
